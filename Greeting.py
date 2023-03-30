ebffc8e3a578e4ce2dc44c5932e8a96399330f4a
waeil5 <128425703+waeil5@users.noreply.github.com>
2023-03-29 23:19:37 -0400

    Main

--- Greeting.py
+++ Greeting.py
@@ -1,4 +1,9 @@
 
 import emoji
-print(emoji.emojize("Hello World :thumbs_up: :face_with_tears_of_joy:
-"))
+print(emoji.emojize("Hello World :thumbs_up: :face_with_tears_of_joy:"))
+
+
+
+name = "Waeil"
+s_name = name[2]
+print (s_name)
